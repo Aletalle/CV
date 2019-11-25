d401a637f332d0fcb410ebb523b2c9ca6608f426
Aletalle <letalle.albin@gmail.com>
2019-11-25 15:47:18 +0100

    CV Web

---
+++ view/body.php
@@ -0,0 +1,295 @@
+<?php
+include("header.php");
+?>
+<!-- Début de la partie profil  -->
+<div class="blueT"> 
+    <section class="container center" id="profil">
+        <h2 class="white-text text-darken-4">Concepteur/Développeur d'applications</h2><br>
+    </section>
+</div>
+<div class="blueC">
+    <!-- Intégration photo -->
+    <section class="container center">
+        <div class="row">
+            <div class="col s12">
+                <img src="../assets/img/lama.jpg" alt="" class=" circle responsive-img">
+            </div>
+    </section>
+    <!-- Fin intégration photo -->
+    <!-- Descriptif profil -->          
+    <section class="container center">
+        <div class="col s12">
+            <h5 class="white-text text-darken-2">Albin Letalle</h5>
+            <h5 class="white-text text-darken-2">4 novembre 1985</h5>
+            <h5 class="white-text text-darken-2">3, rue de Bordeaux Apt3</h5>
+            <h5 class="white-text text-darken-2"> 80300 Albert</h5>
+            <h5 class="white-text text-darken-2">06 59 69 42 35</h5><br>
+            <a class="waves-effect waves-light btn green darken-2" href="mailto:letalle.albin@gmail.com?subject=contact"><i class="material-icons right">mail</i>contact</a>
+        </div>
+    </section>
+    <!-- fin du descriptif profil -->
+</div>
+<!-- fin de la partie profil -->
+<!-- Début de la partie Technologies -->
+<div class="blueCT" id="techno"> 
+    <section class="container">
+        <h2 class="dark-text">Technologies</h2>
+        <p>(liste non exhaustive)</p>
+    </section>
+</div>
+<div class="blueCC">
+    <section class="container">
+        <!-- Début de la partie langages -->
+        <div class="row">
+            <h4 class="light-blue-text text-accent-4">Langages</h4>
+            <div class="col s12 l4">
+                <p>Javascript, HTML 5, Java, CSS 3, MySQL, Android, PHP...</p>
+            </div>
+            <div class="col s12 l8">
+                <div class="col s3 l2">
+                    <div class="card ">
+                        <div class="card-image ">
+                            <img  src="../assets/img/js.png" alt="JavaScript" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img  alt="Html5" src="../assets/img/html5.png" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img alt="Java" src="../assets/img/java.png" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img alt="Css" src="../assets/img/css3.png" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img alt="MySQL" src="../assets/img/mysql.png"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/android.png" alt="android" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/php.png" alt="Php" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- fin de la partie langages -->
+        <!-- Début de la partie frameworks -->
+        <div class="row">
+            <h4 class="light-blue-text text-accent-4">Frameworks</h4>
+            <div class="col s12 l4">JavaFX, jQuery, Bootstrap 4, Materialize 1.0.0, Vue.js...</div>
+            <div class="col s12 l8">
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/javafx.png"alt="Java Fx"/>
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/jquery.png" alt="Jquery" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/bootstrap.png" alt="Bootstrap" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/materialize.png" alt="Materialize" />
+                        </div>
+                    </div>
+                </div>
+                <div class="col s3 l2">
+                    <div class="card">
+                        <div class="card-image">
+                            <img src="../assets/img/vue.png" alt="Vue.js" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- fin de la partie frameworks -->     
+    </section>
+</div>
+<!-- Fin de la partie Technologies -->
+<!-- Début de la partie Etudes -->
+<div class="greyT" id="etudes">
+    <section class="container">
+        <h2 class="dark-text">Etudes</h2>
+    </section>
+</div>
+<div class="greyC">
+    <section class="container">
+        <!-- Début des descriptifs d'études -->
+        <div class="row">
+            <div class="col s12 l6">
+                <h5 class="dark-text">Titre CDA (Concepteur Développeur d'applications)</h5>
+                <ul class="grey-text text-darken-2">
+                    <li>
+                        <strong>AFPA</strong> Amiens
+                    </li>
+                    <li>
+                        De juillet <strong> 2019 </strong> à juin <strong> 2020 </strong>
+                    </li>
+                </ul>
+                <h5 class="dark-text">CQPM Ajusteur Monteur</h5>
+                <ul class="grey-text text-darken-2">
+                    <li>
+                        <strong> Indutrilab</strong> Méaulte
+                    </li>
+                    <li>
+                        De septembre<strong> 2016 </strong>à juin <strong> 2017 </strong>
+                    </li>
+                </ul>
+                <h5 class="dark-text">Formation Bras de mesure 3D Faro et son logiciel Microlog</h5>
+                <ul class="grey-text text-darken-2">
+                    <li>
+                        <strong>Faro France</strong> Bienvillers-au-bois
+                    </li>
+                    <li>
+                        <strong> 2008 </strong>
+                    </li>
+                </ul>
+                <h5 class="dark-text">BTS ROC (Réalisation d'Ouvrages Chaudronnés )</h5>
+                <span class="grey-text text-darken-2"><i>(Apprentissage)</i></span>
+                <ul class="grey-text text-darken-2">
+                    <li>
+                        <strong>CFAI AISNE</strong> Soissons
+                    </li>
+                    <li>
+                        De <strong> 2006 </strong> à <strong> 2009 </strong>
+                    </li>
+                </ul>
+                <h5 class="dark-text">BAC ROCSM (Réalisation d'Ouvrages Chaudronnés et Structures Métalliques)</h5>
+                <span class="grey-text text-darken-2"><i>(Apprentissage)</i></span>
+                <ul class="grey-text text-darken-2">
+                    <li>
+                        <strong>CFAI SOMME</strong> Amiens
+                    </li>
+                    <li>
+                        De <strong> 2004 </strong> à <strong> 2006 </strong>
+                    </li>
+                </ul>
+                <h5 class="dark-text">BEP ROCSM (Réalisation d'Ouvrages Chaudronnés et Structures Métalliques)</h5>
+                <span class="grey-text text-darken-2"><i>(Apprentissage)</i></span>
+                <ul class="grey-text text-darken-2">
+                    <li>
+                        <strong>CFAI SOMME</strong> Amiens
+                    </li>
+                    <li>
+                        De <strong> 2002 </strong> à <strong> 2004 </strong>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Fin  du déscriptif études -->
+    </section>
+</div>
+<!-- Fin de la partie études -->
+<!-- Début de la partie expérience -->
+<div class="greenT" id="experiences">
+    <section class="container">
+        <h2 class="dark-text">Expériences professionnelles</h2>
+    </section>
+</div>
+<div class="greenC">
+    <section class="container">
+        <!-- Début des descriptifs d'expériences -->
+        <div class="row">
+            <div class="col s12 l6">
+                <h5 class="green-text text-darken-2">Ajusteur-monteur de cellules aéronefs</h5>
+                <ul >
+                    <li>
+                        <strong>Stelia AEROSPACE</strong> Méaulte
+                    </li>
+                    <li>
+                        De<strong> 2016 </strong> à <strong> 2018 </strong>
+                    </li>
+                </ul>
+                <h5 class="green-text text-darken-2">Projeteur thermique</h5>
+                <ul>
+                    <li>
+                        <strong> DOUCE HYDRO </strong> Albert
+                    </li>
+                    <li>
+                        De <strong> 2013 </strong>à  <strong> 2016 </strong>
+                    </li>
+                </ul>
+                <h5 class="green-text text-darken-2">Chaudronnier</h5>
+                <ul>
+                    <li>
+                        <strong>FRIEDLANDER</strong> Amiens
+                    </li>
+                    <li>
+                        De <strong> 2010 </strong>à  <strong> 2013 </strong>
+                    </li>
+                </ul>
+                <h5 class="green-text text-darken-2">Contrôleur qualité en mécanique et travail des métaux</h5>
+                <ul>
+                    <li>
+                        <strong>SPM</strong>  Bienvillers-au-Bois
+                    </li>
+                    <li>
+                        De <strong> 2006 </strong> à <strong> 2009 </strong>
+                    </li>
+                </ul>
+                <h5 class="green-text text-darken-2">Chaudronnier</h5>
+                <ul>
+                    <li>
+                        <strong>CSM</strong> Bray-sur-Somme
+                    </li>
+                    <li>
+                        De <strong> 2004 </strong> à <strong> 2006 </strong>
+                    </li>
+                </ul>
+                <h5 class="green-text text-darken-2">Chaudronnier</h5>
+                <ul>
+                    <li>
+                        <strong>SAS Mordacque</strong>  Albert
+                    </li>
+                    <li>
+                        De <strong> 2002 </strong> à <strong> 2004 </strong>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <!-- Fin  du déscriptif d'expériences -->
+    </section>
+</div>
+<!-- Fin de la partie expériences -->
+<?php
+include("footer.php");
+?>
+	
---
+++ view/footer.php
@@ -0,0 +1,12 @@
+<br>
+<footer>
+
+</footer>
+<!-- JQuery -->
+<!--Import jQuery before materialize.js-->
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+<script src="../assets/js/js.js"></script>
+</body>
+</html>
